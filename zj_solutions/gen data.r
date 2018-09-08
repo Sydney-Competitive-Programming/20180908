ddb54d3035f99577082e0721899b64b7eeba5894
susabi <zhuojia.dai@gmail.com>
2018-09-08 14:50:25 +1000

    corrected data

--- zj_solutions/gen data.r
+++ zj_solutions/gen data.r
@@ -4,6 +4,10 @@ r = sample(128, 16*1e6,T)
 mr = matrix(r, ncol=16)
 write.csv(mr, "zj_many_rows_short.csv")
 
+mr1 = data.table(mr)
+fwrite(mr1, "zj_many_rows_short.csv",col.names = F)
+
 r = sample(128, 1e6,T)
 mr = matrix(r, nrow=1)
-write.csv(mr, "zj_1_row_long.csv")
+mr1 = data.table(mr)
+fwrite(mr1, "zj_1_row_long.csv",col.names = F)
--- zj_solutions/generate test cases.jl
+++ zj_solutions/generate test cases.jl
@@ -1,13 +1,25 @@
+# using DataFrames, CSV
+#
+# # generate test cases
+# srand(1)
+# vs = abs.(rand(Int8, 1, 1_000_000))
+# vs_many_rows = abs.(rand(Int8, 1_000, 16))
+#
+#
+# CSV.write("zj_1_long_row.csv",DataFrame(vs), header=false);
+# CSV.write("zj_many_short_rows.csv", DataFrame(vs_many_rows), header=false);
+#
+#
+# vs_soln = waterheight(vs)
+
+
 using DataFrames, CSV
 
-# generate test cases
-srand(1)
-vs = abs.(rand(Int8, 1, 1_000_000))
-vs_many_rows = abs.(rand(Int8, 1_000, 16))
+vs_df = CSV.read("zj_many_rows_short.csv")
 
-
-CSV.write("zj_1_long_row.csv",DataFrame(vs), header=false);
-CSV.write("zj_many_short_rows.csv", DataFrame(vs_many_rows), header=false);
-
-
-vs_soln = waterheight(vs)
+sa = size(vs_df)
+v = Int8[]
+for i in 1:sa[2]
+    push!(v, a[1,i])
+end
+print(v)
